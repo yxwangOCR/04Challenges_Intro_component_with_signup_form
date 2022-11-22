f5497cbf86a9d9c3a0fbcd79bd038f58401a7372
Yixuan WANG <wang.yixuan@live.fr>
2022-11-22 13:47:35 -0500

    Fix h1

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <div className="intro">
-      <h1>Learn to code by watching others</h1>
+    <div className='intro'>
+      <h1>
+        Learn to code by <br />
+        watching others
+      </h1>
       <p>
         See how experienced developers solve problems in real-time. Watching
         scripted tutorials is great, but understanding how developers think is
